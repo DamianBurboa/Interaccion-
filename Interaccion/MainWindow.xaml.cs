f5204f2f656697d8f5f6227b1f1b89ea68771281
DamianBurboa <damianburboa1997@gmail.com>
2019-10-03 23:18:25 -0700

    Add project files.

---
+++ Interaccion/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interaccion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); //verifica que esten iniciados los elementos del interfaz de usuario
+
+            btnBoton.Click +=
+                BtnBoton_Click;
+        }
+
+        private void BtnBoton_Click(object sender, RoutedEventArgs e)
+        {
+            lblHolaMundo.Text = "Adios mundo!";
+            lblHolaMundo.FontSize = 29;
+            lblHolaMundo.Foreground = Brushes.Red;
+        }
+
+        private void BtnBotonSegundo_Click(object sender, RoutedEventArgs e)
+        {
+            lblHolaMundo.Text = "Segundo boton!";
+            lblHolaMundo.FontSize = 18;
+            lblHolaMundo.Foreground = Brushes.Aqua;
+        }
+
+        private void BtnBotonTercero_Click(object sender, RoutedEventArgs e)
+        {
+            lblHolaMundo.Text = "Tercer boton!";
+            lblHolaMundo.FontSize = 35;
+            lblHolaMundo.Foreground = Brushes.Green;
+        }
+
+        private void BtnBoton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
